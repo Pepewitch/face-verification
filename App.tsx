e400194b6e886b04cc4b88843961379e118f523f
Witchayut <skpepe134@gmail.com>
2019-10-03 01:20:45 +0700

    :sparkles: add modal for response showing

--- App.tsx
+++ App.tsx
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components/native";
 import { Text, Platform, StatusBar, TouchableOpacity } from "react-native";
 import { FaceDetectCamera } from "./components/FaceDetectCamera";
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
+import { Provider } from "@ant-design/react-native";
 
 const StyledView = styled.View`
   padding-top: ${Platform.OS === "ios" ? 0 : StatusBar.currentHeight}px;
@@ -9,10 +12,29 @@ const StyledView = styled.View`
   background-color: black;
 `;
 
+const antoutline = require("@ant-design/icons-react-native/fonts/antoutline.ttf");
+const antfill = require("@ant-design/icons-react-native/fonts/antfill.ttf");
+
+const loadFont = async () => {
+  await Font.loadAsync({
+    antoutline,
+    antfill
+  });
+};
+
 export default () => {
+  const [isReady, setIsReady] = useState(false);
+  useEffect(() => {
+    loadFont().then(() => {
+      setIsReady(true);
+    });
+  }, []);
+  if (!isReady) return <AppLoading />;
   return (
-    <StyledView>
-      <FaceDetectCamera />
-    </StyledView>
+    <Provider>
+      <StyledView>
+        <FaceDetectCamera />
+      </StyledView>
+    </Provider>
   );
 };
--- components/FaceDetectCamera.tsx
+++ components/FaceDetectCamera.tsx
@@ -6,12 +6,16 @@ import {
   Dimensions,
   TouchableOpacity,
   Text,
-  CameraRoll
+  CameraRoll,
+  Image
 } from "react-native";
 import { Camera } from "expo-camera";
 import * as FaceDetector from "expo-face-detector";
 import _ from "lodash";
 import * as ImageManipulator from "expo-image-manipulator";
+import { Modal, Button, ActivityIndicator } from "@ant-design/react-native";
+
+const { alert } = Modal;
 
 const StyledCamera = styled(Camera)`
   width: ${props => props.width};
@@ -26,6 +30,7 @@ const StyledTouchableOpacity = styled.TouchableOpacity`
   margin-top: 16px;
   border: 8px solid rgba(255, 255, 255, 0.5);
   align-self: center;
+  opacity: ${props => (props.disabled ? 0.2 : 1)};
 `;
 
 const boundingPad = 8;
@@ -38,9 +43,20 @@ const BoundingBox = styled.View`
   height: ${props => props.face.bounds.size.height + 2 * boundingPad};
   border: ${props =>
     props.highlight
-      ? `4px solid rgba(255, 0, 0, 0.8)`
+      ? props.alignment
+        ? `4px solid rgba(0, 255, 0, 0.8)`
+        : `4px solid rgba(255, 0, 0, 0.8)`
       : `2px solid rgba(0, 0, 255, 0.6)`};
   border-radius: 8px;
+`;
+
+const Backdrop = styled.View`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.7);
 `;
 
 const usePermission = () => {
@@ -62,7 +78,9 @@ const validateFace = face => {
     _.get(face, "bounds.origin.y") &&
     _.get(face, "bounds.origin.x") &&
     _.get(face, "bounds.size.width") &&
-    _.get(face, "bounds.size.height")
+    _.get(face, "bounds.size.height") &&
+    _.get(face, "rollAngle") &&
+    _.get(face, "yawAngle")
   );
 };
 
@@ -90,7 +108,11 @@ const useFaceDetection = () => {
     throttledSetFaces(faces.filter(face => validateFace(face)));
   };
   const detectFace = async imageUri => {
-    const options = { mode: FaceDetector.Constants.Mode.fast };
+    const options = {
+      mode: FaceDetector.Constants.Mode.fast,
+      detectLandmarks: FaceDetector.Constants.Landmarks.none,
+      runClassifications: FaceDetector.Constants.Classifications.none
+    };
     return await FaceDetector.detectFacesAsync(imageUri, options);
   };
   return { faces, onFacesDetected, detectFace };
@@ -100,14 +122,24 @@ const crop = async (uri, bounds) => {
   const actions = [
     {
       crop: {
-        originX: bounds.origin.x - boundingPad,
-        originY: bounds.origin.y - boundingPad,
-        width: bounds.size.width + 2 * boundingPad,
-        height: bounds.size.height + 2 * boundingPad
+        originX: bounds.origin.x,
+        originY: bounds.origin.y,
+        width: bounds.size.width,
+        height: bounds.size.height
       }
     }
   ];
-  const saveOptions = { format: ImageManipulator.SaveFormat.PNG };
+  const saveOptions = { format: ImageManipulator.SaveFormat.JPEG };
+  return ImageManipulator.manipulateAsync(uri, actions, saveOptions);
+};
+
+const resize = async uri => {
+  const actions = [
+    {
+      resize: { width: 1000 }
+    }
+  ];
+  const saveOptions = { format: ImageManipulator.SaveFormat.JPEG };
   return ImageManipulator.manipulateAsync(uri, actions, saveOptions);
 };
 
@@ -129,14 +161,34 @@ export const FaceDetectCamera = () => {
       try {
         const photo = await takePicture();
         if (photo) {
-          const { faces } = await detectFace(photo.uri);
-          const crops = await Promise.all(
-            faces.map(face => crop(photo.uri, face.bounds))
-          );
-          if (hasCameraRollPermission) {
-            await Promise.all(
-              crops.map(croppedImage => save(croppedImage.uri))
+          const resized = await resize(photo.uri);
+          const { faces } = await detectFace(resized.uri);
+          const biggestFace = _.maxBy(faces, face => face.bounds.size.width);
+          if (biggestFace) {
+            const croppedFace = await crop(resized.uri, biggestFace.bounds);
+            alert(
+              "Verify",
+              <Image
+                style={{ width: 300, height: 300 }}
+                resizeMode="contain"
+                source={{ uri: croppedFace.uri }}
+              />,
+              [
+                {
+                  text: "Cancel",
+                  onPress: () => console.log("cancel"),
+                  style: "default"
+                },
+                { text: "OK", onPress: () => console.log("ok") }
+              ]
             );
+            if (hasCameraRollPermission) {
+              await save(croppedFace.uri);
+            }
+          } else {
+            alert("Verify", "Cannot detect face, please try again.", [
+              { text: "OK", onPress: () => console.log("ok") }
+            ]);
           }
         }
       } catch (error) {
@@ -164,12 +216,22 @@ export const FaceDetectCamera = () => {
         {faces.map((face, index) => (
           <BoundingBox
             highlight={face === _.maxBy(faces, face => face.bounds.size.width)}
+            alignment={
+              Math.abs(face.rollAngle) < 15 && Math.abs(face.yawAngle) < 15
+            }
             face={face}
             key={index}
           />
         ))}
+        {isTakingPhoto && <Backdrop />}
       </View>
-      <StyledTouchableOpacity disable={isTakingPhoto} onPress={onPress} />
+      <StyledTouchableOpacity disabled={isTakingPhoto} onPress={onPress} />
+      <ActivityIndicator
+        size="large"
+        toast
+        text="Loading..."
+        animating={isTakingPhoto}
+      />
     </View>
   );
 };
